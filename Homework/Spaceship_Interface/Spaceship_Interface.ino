311269b30f948527caaca8328938c7b537b45c50
aradicaldreamer <31594961+aradicaldreamer@users.noreply.github.com>
2017-10-09 09:32:51 +0100

    Adding code blocks from week 1 homework

---
+++ Homework/Spaceship_Interface/Spaceship_Interface.ino
@@ -0,0 +1,30 @@
+int switchState = 0;
+
+void setup() {
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(2, INPUT);
+}
+
+void loop() {
+  switchState = digitalRead(2);
+  // this reads the data being inputted on pin 2
+
+  if (switchState == LOW) {
+    // the button is not pressed
+    digitalWrite(3, HIGH); // green LED ON
+    digitalWrite(4, LOW); // red LED OFF
+    digitalWrite(5, LOW); // red LED OFF
+    } else {
+      // the button is pressed
+      digitalWrite(3, LOW);
+      digitalWrite(4, LOW);
+      digitalWrite(5, HIGH);
+      delay(250); // wait for a quarter-second
+      // toggle the LEDs
+      digitalWrite(4, HIGH);
+      digitalWrite(5, LOW);
+      delay(250); // wait for a quarter-second
+      }
+} // go back to the beginning of the loop
